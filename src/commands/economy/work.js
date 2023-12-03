64dbe291462b30017e27aade2b134152c2f74828
Korrumz2 <108294523+KorrumzPL@users.noreply.github.com>
2023-12-03 15:59:52 +0100

    ⚡️ | feat(economy): Use sentences in **work.js**

--- src/commands/economy/work.js
+++ src/commands/economy/work.js
@@ -5,31 +5,77 @@ export default {
 
   async execute(client, interaction) {
     const money = Math.floor(Math.random() * (1000 + 1));
+    const actions = ["Win", "Lose"];
+    const action = actions[Math.floor(Math.random() * actions.length)];
 
-    const user = await client.prisma.economy.findFirst({
+    const user = client.prisma.economy.findFirst({
       where: { uid: interaction.user.id },
     });
 
-    await client.prisma.economy.upsert({
-      where: {
-        uid_guildId: {
-          guildId: interaction.guild.id,
-          uid: interaction.user.id,
-        },
-      },
-      create: {
-        guildId: interaction.guild.id,
-        uid: interaction.user.id,
-        wallet: String(money),
-      },
-      update: {
-        wallet: (parseInt(user?.wallet || "0") + money).toString(),
-      },
+    const settings = client.prisma.economy.findFirst({
+      where: { guildId: interaction.guild.id },
     });
 
-    const embedSuccess = new EmbedBuilder()
-      .setDescription(`You worked and got ${money} coins!`)
-      .setColor("DarkGreen");
-    await interaction.reply({ embeds: [embedSuccess] });
+    const getSentences = async (actionType) => {
+      const sentences = await client.prisma.economySettings.findMany({
+        where: {
+          guildId: interaction.guild.id,
+          type: "work",
+          action: actionType,
+        },
+      });
+      return sentences.map((x) => x.sentence);
+    };
+
+    const listWinSentences = await getSentences("win");
+    const listLoseSentences = await getSentences("lose");
+
+    const getRandomSentence = (sentenceList) =>
+      sentenceList[Math.floor(Math.random() * sentenceList.length)];
+
+    const replaceStrWin = getRandomSentence(listWinSentences)
+      .replace(/{user}/g, interaction.user.tag)
+      .replace(/{userid}/g, interaction.user.id)
+      .replace(/{usersOnGuild}/g, interaction.guild.members.cache.size)
+      .replace(/{money}/g, money)
+      .replace(/{currency}/g, settings?.currency || "🌈");
+
+    const replaceStrLose = getRandomSentence(listLoseSentences)
+      .replace(/{user}/g, interaction.user.tag)
+      .replace(/{userid}/g, interaction.user.id)
+      .replace(/{usersOnGuild}/g, interaction.guild.members.cache.size)
+      .replace(/{money}/g, money)
+      .replace(/{currency}/g, settings?.currency || "🌈");
+
+    const updateWallet = async (amount) => {
+      await client.prisma.economy.upsert({
+        where: {
+          uid_guildId: {
+            guildId: interaction.guild.id,
+            uid: interaction.user.id,
+          },
+        },
+        create: {
+          guildId: interaction.guild.id,
+          uid: interaction.user.id,
+          wallet: amount.toString(),
+        },
+        update: { wallet: (parseInt(user?.wallet || "0") + amount).toString() },
+      });
+    };
+
+    if (action === "Win") {
+      await updateWallet(money);
+      const embedSuccess = new EmbedBuilder()
+        .setDescription(`${replaceStrWin}`)
+        .setColor("DarkGreen");
+      await interaction.reply({ embeds: [embedSuccess] });
+    } else {
+      await updateWallet(-money);
+      const embedFail = new EmbedBuilder()
+        .setDescription(`${replaceStrLose}`)
+        .setColor("DarkRed");
+      await interaction.reply({ embeds: [embedFail] });
+    }
   },
 };
