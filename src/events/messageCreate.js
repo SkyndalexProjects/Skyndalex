2386cd44009150acfe8cd48f5f8791a948683c45
Korrumz2 <108294523+KorrumzPL@users.noreply.github.com>
2023-12-09 23:09:01 +0100

    ✨ | feat(AI): Image generation [mega sus, don't try at home, you know what i mean]

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,3 +1,4 @@
+import { AttachmentBuilder } from "discord.js";
 import { config } from "dotenv";
 
 config();
@@ -21,37 +22,68 @@ export default async function messageCreate(client, message) {
     ) {
       if (message.content.startsWith("//")) return;
       await message.channel.sendTyping();
-      try {
-        async function query(data) {
-          const response = await fetch(
-            "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large",
-            {
-              headers: { Authorization: `Bearer ${process.env.HF_TOKEN}` },
-              method: "POST",
-              body: JSON.stringify(data),
-            },
-          );
-          const result = await response.json();
-          return result;
-        }
 
-        query({
-          inputs: {
-            text: message.content,
-            past_user_inputs: [await getLastMessages(10)],
-          },
-        }).then(async (response) => {
-          message.reply(response.generated_text);
-        });
+      try {
+        if (message.content.startsWith("--genimg")) {
+          async function query(data) {
+            const response = await fetch(
+              "https://api-inference.huggingface.co/models/openskyml/dalle-3-xl",
+              {
+                headers: { Authorization: `Bearer ${process.env.HF_TOKEN}` },
+                method: "POST",
+                body: JSON.stringify(data),
+              },
+            );
+            const result = await response.blob();
+            return result;
+          }
+
+          try {
+            const response = await query({ inputs: message.content });
+            console.log(response);
+
+            const imageBuffer = await response.arrayBuffer();
+            const image = new AttachmentBuilder(
+              Buffer.from(imageBuffer),
+              "image.png",
+            );
+
+            await message.reply({ files: [image] });
+          } catch (error) {
+            console.error("Error:", error);
+          }
+        } else {
+          const result = await textGen(message.content);
+          const response = result.generated_text;
+          await message.reply(response);
+        }
       } catch (error) {
-        console.error("Error generating text:", error);
+        console.error("Error while generating:", error);
       }
+
       async function getLastMessages(limit) {
         const messages = await message.channel.messages.fetch({ limit: limit });
         const messagesWithoutBot = messages.filter(
           (message) => !message.author.bot,
         );
         return messagesWithoutBot.map((m) => m.content);
+      }
+
+      async function textGen(input) {
+        const response = await fetch(
+          "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large",
+          {
+            headers: { Authorization: `Bearer ${process.env.HF_TOKEN}` },
+            method: "POST",
+            body: JSON.stringify({
+              inputs: {
+                text: input,
+                past_user_inputs: [await getLastMessages(10)],
+              },
+            }),
+          },
+        );
+        return await response.json();
       }
     }
   }
