e1796ad4c6ead4486cf5d3629c1bb28f68b13da5
KorrumzPL <korrumzprivate@gmail.com>
2024-02-15 23:31:40 +0100

    🐛 fix (4fun): cat, dog

--- src/commands/fun/dog.js
+++ src/commands/fun/dog.js
@@ -3,28 +3,26 @@ import fetch from "node-fetch";
 export default {
   data: new SlashCommandBuilder().setName("dog").setDescription("Random dog"),
 
-  execute(client, interaction) {
-    fetch("https://www.reddit.com/r/dogpictures.json?limit=10000")
-      .then((res) => res.json())
-      .then(async (json) => {
-        let post =
-          json.data.children[
-            Math.floor(Math.random() * json.data.children.length)
-          ].data;
+  async execute(client, interaction) {
+    const response = await fetch("https://www.reddit.com/r/dog.json?limit=10000")
+    const json = await response.json();
 
-        const embed = new EmbedBuilder()
-          .setTitle(`\`r/${post.subreddit}:\` ${post.title}`)
-          .setURL(post.url)
-          .setColor("#3498db");
+    let post =
+      json.data.children[
+        Math.floor(Math.random() * json.data.children.length)
+      ].data;
 
-        if (
-          post.url.endsWith(".jpg") ||
-          post.url.endsWith(".png") ||
-          post.url.endsWith(".gif") ||
-          post.url.endsWith(".jpeg")
-        )
-          embed.setImage(post.url);
-        await interaction.reply({ embeds: [embed] });
-      });
+    const embed = new EmbedBuilder()
+      .setTitle(`\`r/${post.subreddit}:\` ${post.title}`)
+      .setURL(post.url)
+      .setColor("#3498db");
+
+    if (
+      post.url.endsWith(".jpg") ||
+      post.url.endsWith(".png") ||
+      post.url.endsWith(".jpeg")
+    )
+      embed.setImage(post.url);
+    await interaction.reply({ embeds: [embed] });
   },
 };
