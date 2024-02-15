89d8636561059f27dd685174d7a6dfddd32aab4a
KorrumzPL <korrumzprivate@gmail.com>
2024-02-15 22:33:42 +0100

    🐛️ fix (AI) rerun button

--- src/interactions/buttons/ai/rerun.js
+++ src/interactions/buttons/ai/rerun.js
@@ -5,24 +5,29 @@ import {
   EmbedBuilder,
 } from "discord.js";
 import { fetchData } from "../../../events/messageCreate.js";
+import { HfInference } from '@huggingface/inference'
 
+const hf = new HfInference(process.env.HF_TOKEN)
 export default {
   customId: "rerun",
   type: "button",
 
   run: async (client, interaction) => {
     try {
-      const sentMessage = await interaction.reply(
-        "<a:4704loadingicon:1183416396223352852> Re-initialization...",
-      );
-      const prompt = interaction.message.embeds[0].description
-        .split('"')[1]
-        .replaceAll("**", "");
+      console.log(interaction.message.embeds[0])
+      const prompt = interaction.message.embeds[0].description.split("Input: ")[1];
+      const response = await hf.textToImage({
+        inputs: prompt,
+        model: 'stabilityai/stable-diffusion-2-1',
+        parameters: {
+          negative_prompt: 'blurry',
+        },
+        use_cache: false,
+        wait_for_model: true
+      });
 
-      const response = await fetchData(
-        "https://api-inference.huggingface.co/models/openskyml/dalle-3-xl",
-        JSON.stringify({ inputs: prompt }),
-        "blob",
+      const sentMessage = await interaction.reply(
+        "<a:4704loadingicon:1183416396223352852> Re-initialization... (Input: ${prompt})",
       );
 
       if (response?.error) {
