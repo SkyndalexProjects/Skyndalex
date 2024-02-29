b1706179d72f83cec321564af6772097518150c9
KorrumzPL <korrumzprivate@gmail.com>
2024-02-29 20:37:15 +0100

    ⚡️ feat(4fun): add api key to thecatapi and thedogapi

--- src/commands/fun/cat.js
+++ src/commands/fun/cat.js
@@ -5,7 +5,12 @@ export default {
 
   async execute(client, interaction) {
     const response = await fetch(
-      "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1",
+      "https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1", {
+        headers: {
+          "Content-Type": "application/json",
+          "x-api-key": process.env.THECATAPI_KEY,
+        }
+      }
     );
     const json = await response.json();
 
