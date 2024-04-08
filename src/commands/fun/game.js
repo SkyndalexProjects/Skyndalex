a7e67c087c1aad1c182b95e84e1e989462abad06
KorrumzPL <korrumzprivate@gmail.com>
2024-04-08 18:36:56 +0200

    feat(game): autocomplete option, newer `GameDig` version

--- src/commands/fun/game.js
+++ src/commands/fun/game.js
@@ -1,45 +1,41 @@
 import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { GameDig, games } from "gamedig";
+import fetch from "node-fetch";
 export async function run(client, interaction) {
-		// const data = await Gamedig.query({
-		// 	type: interaction.options.getString("game"),
-		// 	host: interaction.options.getString("server"),
-		// });
+	try {
+		const data = await GameDig.query({
+			type: interaction.options.getString("game"),
+			host: interaction.options.getString("server"),
+		});
+		if (!data) return interaction.reply("Server not found.");
+		let embed = new EmbedBuilder()
+			.setTitle(
+				`${data.name} : \`${data.raw.vanilla.connect}\` | Ping: ${data.raw.vanilla.ping}ms`,
+			)
+			.addFields(
+				{
+					name: "Map",
+					value: `${data.map || "None"}`,
+				},
+				{
+					name: "Ping",
+					value: `${data.ping}`,
+				},
+				{
+					name: "Password protected?",
+					value: `${data.raw.vanilla.password}`,
+				},
+				{
+					name: "Players",
+					value: `${data.numplayers}/${data.maxplayers}`,
+				}
+			)
+			.setColor("Green");
 
-		console.log("games", games)
-		// const embed = new EmbedBuilder()
-		// 	.setTitle(
-		// 		`${data.name} : \`${data.raw.vanilla.connect}\` | Ping: ${data.raw.vanilla.ping}ms`,
-		// 	)
-		// 	.addFields(
-		// 		{
-		// 			name: "Map",
-		// 			value: `${data.raw.vanilla.map || "None"}`,
-		// 			inline: true,
-		// 		},
-		// 		{
-		// 			name: "Ping",
-		// 			value: `${data.raw.vanilla.ping}`,
-		// 			inline: true,
-		// 		},
-		// 		{
-		// 			name: "Password protected?",
-		// 			value: `${data.raw.vanilla.password}`,
-		// 			inline: true,
-		// 		},
-		// 	)
-		// 	.setColor("Blurple");
-		//
-		// if (data.players.length > 0) {
-		// 	embed.addFields({
-		// 		name: "Players",
-		// 		value: `${data.players.length}/${data.maxplayers}`,
-		// 		inline: true,
-		// 	});
-		// }
-		//
-		// await interaction.reply({ embeds: [embed] });
-
+		await interaction.reply({ embeds: [embed] });
+	} catch(e) {
+		return interaction.reply({ content: `**${interaction.options.getString("game")}** server not found, or another error. \`(${interaction.options.getString("server")}\`)`, ephemeral: true });
+	}
 }
 export const data = {
 	...new SlashCommandBuilder()
@@ -49,7 +45,8 @@ export const data = {
 			option
 				.setName("game")
 				.setDescription("Game name")
-				.setRequired(true),
+				.setRequired(true)
+				.setAutocomplete(true)
 		)
 		.addStringOption((option) =>
 			option
@@ -60,3 +57,9 @@ export const data = {
 	integration_types: [0, 1],
 	contexts: [0, 1, 2],
 };
+export async function autocomplete(interaction) {
+	const focusedValue = interaction.options.getFocused();
+	const filteredGames = Object.keys(games).filter((game) => game.startsWith(focusedValue));
+	const data = filteredGames.slice(0, 25).map((choice) => ({ name: choice, value: choice }));
+	await interaction.respond(data);
+}
