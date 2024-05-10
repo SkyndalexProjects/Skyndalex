444c18b0889966cb9ca0644ee28a43dc2dac8176
KorrumzPL <korrumzprivate@gmail.com>
2024-05-10 17:40:30 +0200

    remove run functions from indexes

--- src/commands/tools/set/index.ts
+++ src/commands/tools/set/index.ts
@@ -1,16 +1,8 @@
 import {
-	type ChatInputCommandInteraction,
 	SlashCommandBuilder,
 	PermissionFlagsBits,
 } from "discord.js";
-import type { SkyndalexClient } from "../../../classes/Client";
 
-export async function run(
-	client: SkyndalexClient,
-	interaction: ChatInputCommandInteraction,
-) {
-	return interaction.reply("config test");
-}
 export const data = new SlashCommandBuilder()
 	.setName("set")
 	.setDescription("Guild configuration")
