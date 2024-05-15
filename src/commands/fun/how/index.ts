5a3830850873f90d2608f19374c88be9009fc5e2
KorrumzPL <korrumzprivate@gmail.com>
2024-05-15 20:14:04 +0200

    fix: user apps

--- src/commands/fun/how/index.ts
+++ src/commands/fun/how/index.ts
@@ -1,5 +1,9 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export const data = new SlashCommandBuilder()
+export const data = {
+	...new SlashCommandBuilder()
 	.setName("how")
-	.setDescription("how someone is [...]");
+	.setDescription("how someone is [...]"),
+	integration_types: [0, 1],
+	contexts: [0, 1, 2],
+}
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,67 +1,77 @@
-import type {
-	SlashCommandSubcommandBuilder,
-	SlashCommandSubcommandGroupBuilder,
+import {
+	SlashCommandBuilder,
+    type SlashCommandSubcommandBuilder,
+    SlashCommandSubcommandGroupBuilder,
+    Routes
 } from "discord.js";
 import type { SkyndalexClient } from "../classes/Client";
+
 export async function ready(client: SkyndalexClient) {
-	// TODO: rewrite this for a better quality, use recursion functions
-	const parsedCommands = [];
+    const parsedCommands = [];
 
-	for (const [key, value] of client.commands) {
-		if (key.includes("/")) {
-			if (!key.includes("index") || key.split("/").length > 2) continue;
+    for (const [key, value] of client.commands) {
+        if (key.includes("/")) {
+            if (!key.includes("index") || key.split("/").length > 2) continue;
 
-			const [commandName] = key.split("/");
+            const [commandName] = key.split("/");
 
-			const subcommands = client.commands.filter(
-				(x, v) =>
-					v.startsWith(`${commandName}/`) && !v.includes("index"),
-			);
+            const subcommands = client.commands.filter(
+                (x, v) =>
+                    v.startsWith(`${commandName}/`) && !v.includes("index")
+            );
 
-			const command = value.data;
+            const command = value.data as SlashCommandBuilder;
 
-			for (const [key, value] of subcommands.entries()) {
-				if (key.split("/").length <= 2) {
-					command.addSubcommand(
-						value.data as unknown as SlashCommandSubcommandBuilder,
-					);
-				} else {
-					const groupName = key.split("/")[1];
+            for (const [key, value] of subcommands.entries()) {
+                if (key.split("/").length <= 2) {
+                    if (command instanceof SlashCommandBuilder) {
+                        command.addSubcommand(
+                            value.data as unknown as SlashCommandSubcommandBuilder,
+                        );
+                    }
+                } else {
+                    const groupName = key.split("/")[1];
 
-					const groups = client.commands.filter(
-						(x, v) =>
-							v.startsWith(`${commandName}/${groupName}`) &&
-							!v.includes("index"),
-					);
+                    const groups = client.commands.filter(
+                        (x, v) =>
+                            v.startsWith(`${commandName}/${groupName}`) &&
+                            !v.includes("index")
+                    );
 
-					const groupIndex = client.commands.get(
-						`${commandName}/${groupName}/index`,
-					);
+                    const groupIndex = client.commands.get(
+                        `${commandName}/${groupName}/index`
+                    );
+                    if (!groupIndex) continue;
 
-					if (!groupIndex) continue;
+                    if (groupIndex.data instanceof SlashCommandSubcommandGroupBuilder) {
+                        for (const [gk, gv] of groups.entries()) {
+                            groupIndex.data.addSubcommand(
+                                gv.data as unknown as SlashCommandSubcommandBuilder
+                            );
+                        }
 
-					for (const [gk, gv] of groups.entries()) {
-						groupIndex?.data.addSubcommand(
-							gv.data as unknown as SlashCommandSubcommandBuilder,
-						);
-					}
+                        if (command instanceof SlashCommandBuilder) {
+                            command.addSubcommandGroup(
+                                groupIndex.data as unknown as SlashCommandSubcommandGroupBuilder
+                            );
+                        }
+                    }
+                }
+            }
+            parsedCommands.push(command);
+        } else {
+            parsedCommands.push(value.data);
+        }
+    }
 
-					command.addSubcommandGroup(
-						groupIndex.data as unknown as SlashCommandSubcommandGroupBuilder,
-					);
-				}
-			}
-			parsedCommands.push(command.toJSON());
-		} else {
-			parsedCommands.push(value.data);
-		}
-	}
+    const globalData = await client.rest.put(
+        Routes.applicationCommands(client.user.id),
+        { body: parsedCommands },
+    ) as Map<string, unknown>;
 
-	const globalData = await client.application.commands.set(parsedCommands);
-
-	client.logger.success(
-		`Ready in ${((performance.now() - client.createdAt) / 1000).toFixed(
-			2,
-		)}s (commands: ${globalData.size})`,
-	);
+    client.logger.success(
+        `Ready in ${((performance.now() - client.createdAt) / 1000).toFixed(
+            2,
+        )}s (commands: ${globalData.size})`,
+    );
 }
