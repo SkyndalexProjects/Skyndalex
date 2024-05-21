6c43d161b9ae2d2a0a6d31e738d4de3f4339aabd
KorrumzPL <korrumzprivate@gmail.com>
2024-05-21 17:53:09 +0200

    feat: leave command

---
+++ src/commands/music/leave.ts
@@ -0,0 +1,53 @@
+import {
+	type ChatInputCommandInteraction,
+	SlashCommandBuilder,
+} from "discord.js";
+import type { SkyndalexClient } from "../../classes/Client";
+import { EmbedBuilder } from "../../classes/builders/EmbedBuilder";
+import { getVoiceConnection } from "@discordjs/voice";
+
+export async function run(
+	client: SkyndalexClient,
+	interaction: ChatInputCommandInteraction,
+) {
+	try {
+		await interaction.deferReply();
+		const station = interaction.options.getString("station");
+		const memberChannel = interaction.member.voice.channel;
+
+		if (!memberChannel) {
+			return interaction.editReply({
+				content: `${client.i18n.t("RADIO_JOIN_VOICE_CHANNEL", {
+					lng: interaction.locale,
+				})}`,
+				ephemeral: true,
+			});
+		}
+        
+        const connection = getVoiceConnection(memberChannel.guild.id);
+        if (connection) {
+            connection.destroy() 
+        } else {
+            return interaction.editReply({
+                content: `${client.i18n.t("VOICE_NOT_CONNECTED", {
+                    lng: interaction.locale,
+                })}`,
+                ephemeral: true,
+            });
+        }
+
+        const embed = new EmbedBuilder(client, interaction.locale)
+        .setDescription("VOICE_CHANNEL_LEFT")
+        .setColor("DarkBlue")
+
+        return await interaction.editReply({
+            embeds: [embed]
+        })
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+export const data = new SlashCommandBuilder()
+	.setName("leave")
+	.setDescription("Leave voice channel")
