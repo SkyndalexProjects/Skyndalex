d1754e14e3db93c322da1829f84e16f5263454fa
KorrumzPL <korrumzprivate@gmail.com>
2024-05-23 19:39:48 +0200

    feat(Fun): cat

---
+++ src/commands/fun/cat.ts
@@ -0,0 +1,30 @@
+import {
+	type ChatInputCommandInteraction,
+	SlashCommandBuilder,
+} from "discord.js";
+import type { SkyndalexClient } from "../../classes/Client";
+import { EmbedBuilder } from "classes/builders/EmbedBuilder";
+export async function run(
+	client: SkyndalexClient,
+	interaction: ChatInputCommandInteraction,
+) {
+    try {
+        await interaction.deferReply();
+        const response = await fetch("http://aws.random.cat/meow");
+        const cat = await response.json();
+
+        console.log("cat", cat);
+        const embed = new EmbedBuilder(client, interaction.locale)
+            .setTitle("FUN_CAT_TITLE")
+            .setImage(cat.file)
+
+        await interaction.editReply({ embeds: [embed] });
+    } catch (error) {
+        console.error(error);
+        await interaction.editReply("The API is not available at the moment. Please try again later.");
+    }
+}
+
+export const data = new SlashCommandBuilder()
+	.setName("cat")
+	.setDescription("Generate random cat")
--- src/types/structures.ts
+++ src/types/structures.ts
@@ -71,3 +71,6 @@ export interface LocaleFieldOptions {
 export interface ainasepicsAPI {
 	url: string;
 }
+export interface randomDog {
+	url: string;
+}
