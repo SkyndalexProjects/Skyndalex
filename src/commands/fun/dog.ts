f1792d43ca29adaa9294d4315f55f7dfd15da7e8
KorrumzPL <korrumzprivate@gmail.com>
2024-05-23 19:39:41 +0200

    feat(Fun): dog

---
+++ src/commands/fun/dog.ts
@@ -0,0 +1,25 @@
+import {
+	type ChatInputCommandInteraction,
+	SlashCommandBuilder,
+} from "discord.js";
+import type { SkyndalexClient } from "../../classes/Client";
+import { EmbedBuilder } from "classes/builders/EmbedBuilder";
+import type { randomDog } from "types/structures";
+export async function run(
+	client: SkyndalexClient,
+	interaction: ChatInputCommandInteraction,
+) {
+    await interaction.deferReply()
+    const response = await fetch("https://random.dog/woof.json");
+    const dog = await response.json() as randomDog
+
+    const embed = new EmbedBuilder(client, interaction.locale)
+    .setTitle("FUN_DOG_TITLE")
+    .setImage(dog.url)
+    .setColor("Random")
+    return interaction.editReply({ embeds: [embed] })
+}
+
+export const data = new SlashCommandBuilder()
+	.setName("dog")
+	.setDescription("Generate random dog")
