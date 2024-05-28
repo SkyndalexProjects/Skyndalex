1ae0507aea110fedeabaeef0cfebe2e4f732d527
KorrumzPL <korrumzprivate@gmail.com>
2024-05-28 19:00:55 +0200

    feat(Tickets): Creating custom selects

--- src/components/selectMenus/ticketSelectCategory.ts
+++ src/components/selectMenus/ticketSelectCategory.ts
@@ -1,19 +1,32 @@
+import type { SkyndalexClient } from "classes/Client";
 import {
-	type ChatInputCommandInteraction,
-	SlashCommandSubcommandBuilder,
-	ButtonStyle,
-	ButtonBuilder,
 	ActionRowBuilder,
+	type StringSelectMenuInteraction,
+	ButtonBuilder,
+	ButtonStyle,
 	StringSelectMenuBuilder,
 	StringSelectMenuOptionBuilder,
 } from "discord.js";
-import { EmbedBuilder } from "classes/builders/EmbedBuilder";
-import type { SkyndalexClient } from "../../../classes/Client";
-
 export async function run(
 	client: SkyndalexClient,
-	interaction: ChatInputCommandInteraction,
+	interaction: StringSelectMenuInteraction,
 ) {
+	const option = interaction.values[0].split("-")[1];
+
+	const buttonsFromSelect = await client.prisma.ticketButtons.findMany({
+		where: {
+			guildId: interaction.guild.id,
+			assignedToSelect: `ticketSelectCategory-${option}`,
+		},
+	});
+
+	if (buttonsFromSelect.length <= 0)
+		return interaction.reply({
+			content: client.i18n.t("NO_CUSTOM_BUTTONS_FOUND", {
+				lng: interaction.locale,
+			}),
+		});
+
 	const selects = await client.prisma.ticketSelects.findMany({
 		where: {
 			guildId: interaction.guild.id,
@@ -22,11 +35,6 @@ export async function run(
 
 	const selectActionRow = new ActionRowBuilder<StringSelectMenuBuilder>();
 	const buttonActionRow = new ActionRowBuilder<ButtonBuilder>();
-
-	const embed = new EmbedBuilder(client, interaction.locale)
-		.setTitle("TICKETS_MENU_TITLE")
-		.setDescription("TICKETS_MENU_DESCRIPTION")
-		.setColor("Blue");
 
 	if (selects.length > 0) {
 		const selectOptions = selects.map((select) => {
@@ -42,11 +50,6 @@ export async function run(
 			.addOptions(selectOptions);
 
 		selectActionRow.addComponents(selectBuilder);
-
-		return interaction.reply({
-			embeds: [embed],
-			components: [selectActionRow],
-		});
 	}
 
 	const styles = {
@@ -56,21 +59,7 @@ export async function run(
 		DANGER: ButtonStyle.Danger,
 	};
 
-	const buttons = await client.prisma.ticketButtons.findMany({
-		where: {
-			guildId: interaction.guild.id,
-			assignedToSelect: null,
-		},
-	});
-
-	if (buttons.length <= 0)
-		return interaction.reply({
-			content: client.i18n.t("NO_CUSTOM_BUTTONS_FOUND", {
-				lng: interaction.locale,
-			}),
-		});
-
-	for (const button of buttons) {
+	for (const button of buttonsFromSelect) {
 		const buttonBuilder = new ButtonBuilder()
 			.setStyle(styles[button.style])
 			.setLabel(button.label)
@@ -78,12 +67,7 @@ export async function run(
 		buttonActionRow.addComponents(buttonBuilder);
 	}
 
-	return interaction.reply({
-		embeds: [embed],
-		components: [buttonActionRow],
-	});
+	return interaction.update({
+		components: [buttonActionRow, selectActionRow],
+	})
 }
-
-export const data = new SlashCommandSubcommandBuilder()
-	.setName("menu")
-	.setDescription("Tickets menu");
