d22afbf7012cae22a8c57c51182bbea48dad8ca3
KorrumzPL <korrumzprivate@gmail.com>
2024-05-28 19:01:05 +0200

    feat(Tickets): Assigning buttons to selects

--- src/commands/tools/set/tickets/add-button.ts
+++ src/commands/tools/set/tickets/add-button.ts
@@ -1,7 +1,9 @@
 import {
 	type ChatInputCommandInteraction,
 	SlashCommandSubcommandBuilder,
+	type AutocompleteInteraction,
 	ChannelType,
+	type Channel,
 } from "discord.js";
 import type { SkyndalexClient } from "../../../../classes/Client";
 import { EmbedBuilder } from "classes/builders/EmbedBuilder";
@@ -11,10 +13,24 @@ export async function run(
 ) {
 	const label = interaction.options.getString("label");
 	const style = interaction.options.getString("style");
+	const select = interaction.options.getString("assign-to-select");
 
-	const channel = await interaction.guild.channels.create({
-		name: label,
-		type: ChannelType.GuildCategory,
+	console.log(select.split("-")[1]);
+
+	let channel: Channel;
+
+	if (!select) {
+		channel = await interaction.guild.channels.create({
+			name: label,
+			type: ChannelType.GuildCategory,
+		});
+	}
+
+	const getSelect = await client.prisma.ticketSelects.findFirst({
+		where: {
+			guildId: interaction.guild.id,
+			customId: Number.parseInt(select.split("-")[1]),
+		},
 	});
 
 	await client.prisma.ticketButtons.create({
@@ -22,7 +38,8 @@ export async function run(
 			label,
 			style,
 			guildId: interaction.guild.id,
-			discordChannelId: channel.id,
+			discordChannelId: channel?.id || getSelect?.discordChannelId,
+			assignedToSelect: select,
 		},
 	});
 
@@ -57,4 +74,32 @@ export const data = new SlashCommandSubcommandBuilder()
 				{ name: "Danger", value: "DANGER" },
 			)
 			.setRequired(true),
+	)
+	.addStringOption((option) =>
+		option
+			.setName("assign-to-select")
+			.setDescription("Assign button to select category")
+			.setAutocomplete(true),
 	);
+
+export async function autocomplete(interaction: AutocompleteInteraction) {
+	const focusedValue = interaction.options.getFocused(true).value || "";
+	const categories = await (
+		interaction.client as SkyndalexClient
+	).prisma.ticketSelects.findMany({
+		where: {
+			guildId: interaction.guild.id,
+		},
+	});
+
+	const filteredCategories = categories.filter((category) =>
+		category.label.startsWith(focusedValue),
+	);
+
+	const data = filteredCategories.map((category) => ({
+		name: `${category.label} (ID: ${category.customId})`,
+		value: `ticketSelectCategory-${category.customId}`,
+	}));
+
+	await interaction.respond(data);
+}
