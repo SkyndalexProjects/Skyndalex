afb760964ca59bece0fbc897d2fa8fc86e849736
KorrumzPL <korrumzprivate@gmail.com>
2024-06-14 22:50:20 +0200

    feat: info command

---
+++ src/commands/info/info.ts
@@ -0,0 +1,37 @@
+import {
+	type ChatInputCommandInteraction,
+	SlashCommandBuilder,
+} from "discord.js";
+import type { SkyndalexClient } from "#classes";
+import { EmbedBuilder } from "#builders";
+import os from "node:os";
+
+export async function run(
+	client: SkyndalexClient,
+	interaction: ChatInputCommandInteraction,
+) {
+    const botUptimeTimestamp = `<t:${Math.round(
+		client.readyTimestamp / 1000,
+	)}:R>`;
+
+	const serverUptimeTimestamp = `<t:${Math.floor(
+		Math.floor(Date.now() / 1000 - os.uptime()),
+	)}:R>`;
+
+	const embed = new EmbedBuilder(client, interaction.locale)
+    .setDescription("BOT_INFO_DESCRIPTION", {
+        version: process.env.npm_package_version,
+    })
+    .addFields([
+        { name: "BOT_INFO_CACHE_STATS", value: `- Users: ${client.users.cache.size}\n- Guilds: ${client.guilds.cache.size}\n- Channels: ${client.channels.cache.size}`, inline: true },
+        { name: "BOT_INFO_PROCESS_STATS", value: `- Bot uptime: ${botUptimeTimestamp}\n- System uptime: ${serverUptimeTimestamp}\n- Used memory (rss): ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB`, inline: true },
+        { name: "BOT_INFO_LAVALINK_STATS", value: `- Players: ${client.shoukaku.players.size}\n- Nodes: ${client.shoukaku.nodes.size}`},
+        
+    ])
+    .setColor("Blurple")
+    return interaction.reply({ embeds: [embed] });
+
+}
+export const data = new SlashCommandBuilder()
+	.setName("info")
+	.setDescription("Bot info");
