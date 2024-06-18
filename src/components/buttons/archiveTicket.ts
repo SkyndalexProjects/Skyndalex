ed7e5c0fafd30b2641aba84a90767950626cff2d
KorrumzPL <korrumzprivate@gmail.com>
2024-06-18 20:21:06 +0200

    feat(Tickets): archiving

---
+++ src/components/buttons/archiveTicket.ts
@@ -0,0 +1,49 @@
+import type { SkyndalexClient } from "#classes";
+import { EmbedBuilder, ButtonBuilder } from "#builders";
+import { ActionRowBuilder, ButtonStyle,type MessageComponentInteraction } from "discord.js";
+
+export async function run(
+	client: SkyndalexClient,
+	interaction: MessageComponentInteraction<'cached'>,
+) {
+    const [ticketId, userId] = interaction.customId.split("-").slice(1);
+   
+    const ticket = await client.tickets.archive(
+        interaction.guild.id,
+        userId,
+        Number.parseInt(ticketId),
+    )
+
+    const reopen = new ActionRowBuilder<ButtonBuilder>().addComponents(
+        new ButtonBuilder(client, interaction.locale)
+            .setCustomId(`reopenTicket-${ticketId}-${userId}-${ticket.ticketCategory}`)
+            .setLabel("TICKET_REOPEN")
+            .setStyle(ButtonStyle.Primary)
+    );
+
+    const embed = new EmbedBuilder(client, interaction.locale)
+        .setTitle("TICKET_CREATED_TITLE")
+        .setDescription("TICKET_CREATED_DESCRIPTION", {
+            user: interaction.user.username,
+            category: interaction.channel?.name,
+            state: "closed",
+            id: ticketId,
+        })
+        .setColor("Green")
+    
+    await interaction.update({
+        embeds: [embed],
+        components: [reopen],
+    })
+
+    const settings = await client.prisma.settings.findFirst({
+        where: {
+            guildId: interaction.guild.id,
+        },
+    });
+
+    console.log("settings", settings)
+
+    // @ts-expect-error
+    await interaction.channel?.setParent(settings?.ticketArchiveCategory)
+}
