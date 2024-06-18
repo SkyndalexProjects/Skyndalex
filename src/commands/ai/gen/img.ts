f90ae9e52772100b17042b26d6e442a1ee5d4dd3
KorrumzPL <korrumzprivate@gmail.com>
2024-06-19 00:39:10 +0200

    feat(AI): queue system

--- src/commands/ai/gen/img.ts
+++ src/commands/ai/gen/img.ts
@@ -9,15 +9,18 @@ import type { HuggingFaceImage, HuggingFaceSearchResult } from "#types";
 import { HfInference } from "@huggingface/inference";
 const hf = new HfInference(process.env.HF_TOKEN);
 
+const imageQueue = new Map();
+
 export async function run(
 	client: SkyndalexClient,
 	interaction: ChatInputCommandInteraction,
 ) {
-	await interaction.deferReply();
-
 	const prompt = interaction.options.getString("prompt");
 	const defaultModel = "stabilityai/stable-diffusion-2-1";
 	const model = interaction?.options?.getString("model") || defaultModel;
+
+	const queuePosition = imageQueue.size + 1;
+	const taskId = `${interaction.user.id}-${Date.now()}`;
 
 	if (model !== defaultModel) {
 		// @ts-ignore
@@ -29,33 +32,66 @@ export async function run(
 		}
 	}
 
-	const response = await hf.textToImage({
-		inputs: prompt,
-		model: model,
-		parameters: {
-			negative_prompt: "blurry",
-		},
-		// use_cache: false,
-		// wait_for_model: true,
-	});
+	if (!imageQueue.has(interaction.user.id)) {
+		imageQueue.set(taskId, {
+			status: "queued",
+			position: queuePosition,
+			input: prompt,
+		});
 
-	const imageBuffer =
-		(await response.arrayBuffer()) as HuggingFaceImage["generatedImage"];
-	const image = new AttachmentBuilder(Buffer.from(imageBuffer));
-
-	const embed = new EmbedBuilder(client, interaction.locale)
-		.setDescription(
-			client.i18n.t("IMG_GENERATED", {
-				lng: interaction.locale,
+		const queueMessage = new EmbedBuilder(client, interaction.locale)
+			.setColor("#3498db")
+			.setDescription("IMG_PROCESSING", {
+				queuePosition,
 				prompt,
-				author: interaction.user.username,
+			})
+			.setTimestamp()
+			.setFooter({
+				text: "Powered by Huggingface using Skyndalex bot",
+			});
+
+		const queueReply = await interaction.reply({
+			embeds: [queueMessage],
+		});
+
+		const response = await hf.textToImage({
+			inputs: prompt,
+			model: model,
+			parameters: {
+				negative_prompt: "blurry",
+			},
+			// use_cache: false,
+			// wait_for_model: true,
+		});
+
+		const imageBuffer =
+			(await response.arrayBuffer()) as HuggingFaceImage["generatedImage"];
+		const image = new AttachmentBuilder(Buffer.from(imageBuffer));
+
+		const embed = new EmbedBuilder(client, interaction.locale)
+			.setDescription(
+				client.i18n.t("IMG_GENERATED", {
+					lng: interaction.locale,
+					prompt,
+					author: interaction.user.username,
+				}),
+			)
+			.setFooter({
+				text: "IMG_GENERATED_NSFW_WARNING",
+			})
+			.setColor("Green");
+
+		await interaction.editReply({
+			embeds: [embed],
+			files: [image],
+		});
+		await interaction.followUp({
+			content: client.i18n.t("IMAGE_READY", {
+				userId: interaction.user.id,
 			}),
-		)
-		.setFooter({
-			text: "IMG_GENERATED_NSFW_WARNING",
-		})
-		.setColor("Blue");
-	return interaction.editReply({ embeds: [embed], files: [image] });
+		});
+	}
+	imageQueue.delete(taskId);
 }
 export async function autocomplete(interaction: AutocompleteInteraction) {
 	const focusedValue = interaction.options.getFocused(true).value;
