1e5d5a4b74eeb4101166cb45e6e7ae14d5991f11
KorrumzPL <korrumzprivate@gmail.com>
2024-07-07 13:00:32 +0200

    delete ticket system in database

---
+++ prisma/migrations/20240707110011_delete_ticket_system/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the `ticketButtons` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ticketModals` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `ticketSelects` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `tickets` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "ticketButtons";
+
+-- DropTable
+DROP TABLE "ticketModals";
+
+-- DropTable
+DROP TABLE "ticketSelects";
+
+-- DropTable
+DROP TABLE "tickets";
