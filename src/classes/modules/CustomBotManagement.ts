3f7c60d5bedc7d926f63d1702098c59ca09f0d7f
KorrumzPL <korrumz2@gmail.com>
2024-08-04 14:33:40 +0200

    refactor: remove updatePowerState (customBots)

--- src/classes/modules/CustomBotManagement.ts
+++ src/classes/modules/CustomBotManagement.ts
@@ -49,60 +49,6 @@ export class CustomBotManagement {
 				});
 		}
 	}
-	async updatePowerState(id: string, userId: string, powerState: string) {
-		const custombot = await this.client.prisma.custombots.findUnique({
-			where: {
-				id_userId: {
-					id: Number.parseInt(id),
-					userId,
-				},
-			},
-		});
-
-		const bot = await this.client.users.fetch(custombot.clientId);
-
-		switch (powerState) {
-			case "working":
-				return this.client.prisma.custombots.update({
-					where: {
-						id_userId: {
-							id: custombot.id,
-							userId,
-						},
-					},
-					data: {
-						status: "working",
-					},
-				});
-				break;
-			case "error":
-				return this.client.prisma.custombots.update({
-					where: {
-						id_userId: {
-							id: custombot.id,
-							userId,
-						},
-					},
-					data: {
-						status: "error",
-					},
-				});
-				break;
-			case "offline":
-				return this.client.prisma.custombots.update({
-					where: {
-						id_userId: {
-							id: custombot.id,
-							userId,
-						},
-					},
-					data: {
-						status: "offline",
-					},
-				});
-				break;
-		}
-	}
 	async findCustomBot(id: string, userId: string) {
 		return this.client.prisma.custombots.findUnique({
 			where: {
