9d5e7a7380d6e821bbebd4979ca99589bf25f22f
KorrumzPL <korrumz2@gmail.com>
2024-08-12 19:07:37 +0200

    update migrations

---
+++ prisma/migrations/20240812170727_settings_fix/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ticketArchiveCategory` on the `settings` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "settings" DROP COLUMN "ticketArchiveCategory";
