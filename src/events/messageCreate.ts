146eae0e4745bfdb5eb6ba945f622fb9b658b584
KorrumzPL <korrumz2@gmail.com>
2024-09-28 15:57:11 +0200

    feat(Chatbot): Creating threads

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,114 +1,180 @@
-import { type Message, type BaseGuildTextChannel, MessageActivityType } from "discord.js";
+import {
+	type Message,
+	type BaseGuildTextChannel,
+	ThreadAutoArchiveDuration,
+	ChannelType,
+	Attachment,
+} from "discord.js";
 import type { SkyndalexClient } from "#classes";
 import type { GroqResponse } from "#types";
 
 export async function messageCreate(client: SkyndalexClient, message: Message) {
 	const settings = await client.prisma.settings.findFirst({
-		where: {
-			guildId: message.guild.id,
-		},
+		where: { guildId: message.guild.id },
 	});
 
-	if (settings?.chatbotChannel && settings?.chatbotAPIKey) {
-		if (message.channel.id === settings?.chatbotChannel) {
-			if (message.author.bot) return;
+	if (
+		!settings?.chatbotChannel ||
+		!settings?.chatbotAPIKey ||
+		message.author.bot
+	)
+		return;
 
-			const url = "https://api.groq.com/openai/v1/chat/completions";
+	const isChatbotChannel = message.channel.id === settings.chatbotChannel;
+	const apiUrl = "https://api.groq.com/openai/v1/chat/completions";
+	const maxLength = 2000;
 
-			(message.channel as BaseGuildTextChannel).sendTyping();
+	if (isChatbotChannel || message.channel.type === ChannelType.PublicThread) {
+		const channel = message.channel as BaseGuildTextChannel;
+		channel.sendTyping();
 
-			const response = await fetch(url, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-					Authorization: `Bearer ${settings?.chatbotAPIKey}`,
-				},
-				body: JSON.stringify({
-					model: "llama3-70b-8192",
-					messages: [
-						{
-							role: "system",
-							content: settings?.chatBotSystemPrompt,
-						},
-						{ role: "user", content: message.content },
-					],
-					max_tokens: settings?.chatBotMaxTokens,
-					temperature: settings?.chatBotTemperature,
-					stream: false,
-				}),
-			});
-			const json = (await response.json()) as GroqResponse;
+		const content = await getChatbotResponse(
+			apiUrl,
+			settings,
+			message.content,
+		);
+		if (!content) return;
 
-			if (json.error) {
-				return message.reply(
-					`An error occurred: \`${
-						json.error.message || json?.error
-					}\``,
-				);
-			}
+		if (
+			message.attachments.size > 0 &&
+			isValidImage(message.attachments.first()?.contentType)
+		) {
+			const imageContent = await analyzeImage(
+				apiUrl,
+				settings,
+				content,
+				new URL(message.attachments.first()!.url),
+			);
+			return message.reply(
+				imageContent || "I couldn't analyze this image.",
+			);
+		}
 
-			const responseContent = json.choices[0]?.message?.content || "";
-			const maxLength = 2000;
+		await sendResponse(message, content, maxLength);
 
-			if (
-				message.attachments.size > 0 &&
-				["image/png", "image/jpeg", "image/jpg"].includes(message.attachments.first().contentType || "")
-			) {
-				// llama-3.2-11b-vision-preview
-
-				const response = await fetch(url, {
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${settings?.chatbotAPIKey}`,
-					},
-					body: JSON.stringify({
-						model: "llama-3.2-11b-vision-preview",
-						messages: [
-							{
-								role: "user",
-								content: [
-									{
-										type: "text",
-										text: message?.content || "What is this image?",
-									},
-									{
-										type: "image_url",
-										image_url: {
-											url: message.attachments.first()
-												?.url,
-										},
-									},
-								],
-							},
-							{
-								role: "assistant",
-								content: `Describe the image in the image above. Adjust your description to the user's input language.`,
-							},
-						],
-						max_tokens: 2048,
-						stream: false,
-					}),
-				});
-				const json = (await response.json()) as GroqResponse;
-
-				if (json.error)
-					return message.reply(
-						`Oops! I couldn't analyze this image. Here is full error: \`${json.error.message}\``,
-					);
-
-				message.reply(
-					`${json.choices[0]?.message?.content || "I couldn't analyze this image."}`,
-				);
-			} else {
-				if (responseContent.length > maxLength) {
-					message.reply(
-						`${responseContent.slice(0, maxLength - 3)} [...]`,
-					);
-				} else {
-					message.reply(responseContent);
-				}
+		if (isChatbotChannel && message.reference?.messageId) {
+			const referencedMessage = await message.channel.messages.fetch(
+				message.reference.messageId,
+			);
+			if (referencedMessage.author.id === client.user.id) {
+				await createThreadAndReply(channel, message, content);
 			}
 		}
 	}
+}
+interface Settings {
+	chatbotAPIKey: string;
+	chatBotSystemPrompt?: string;
+	chatBotMaxTokens?: number;
+	chatBotTemperature?: number;
+	chatbotChannel?: string;
+}
+async function getChatbotResponse(
+	apiUrl: string,
+	settings: Settings,
+	content: string,
+): Promise<string | null> {
+	const response = await fetch(apiUrl, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${settings.chatbotAPIKey}`,
+		},
+		body: JSON.stringify({
+			model: "llama3-70b-8192",
+			messages: [
+				{ role: "system", content: settings?.chatBotSystemPrompt },
+				{ role: "user", content },
+			],
+			max_tokens: settings?.chatBotMaxTokens,
+			temperature: settings?.chatBotTemperature,
+		}),
+	});
+
+	const json = (await response.json()) as GroqResponse;
+	if (json.error) {
+		return json.error.message;
+	}
+
+	return json.choices[0]?.message?.content;
+}
+
+async function analyzeImage(
+	apiUrl: string,
+	settings: Settings,
+	content: string,
+	attachment: URL,
+): Promise<string | null> {
+	const response = await fetch(apiUrl, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${settings.chatbotAPIKey}`,
+		},
+		body: JSON.stringify({
+			model: "llama-3.2-11b-vision-preview",
+			messages: [
+				{
+					role: "user",
+					content: [
+						{
+							type: "text",
+							text: content || "What is this image?",
+						},
+						{
+							type: "image_url",
+							image_url: {
+								url: attachment,
+							},
+						},
+					],
+				},
+				{
+					role: "assistant",
+					content:
+						"Describe the image in the image above. Adjust your description to the user's input language.",
+				},
+			],
+			max_tokens: 2048,
+		}),
+	});
+
+	const json = (await response.json()) as GroqResponse;
+	if (json.error)
+		return `Oops! I couldn't analyze this image. Full error: \`${json.error.message}\``;
+
+	return json.choices[0]?.message?.content;
+}
+
+async function sendResponse(
+	message: Message,
+	content: string,
+	maxLength: number,
+) {
+	if (content.length > maxLength) {
+		await message.reply(`${content.slice(0, maxLength - 3)} [...]`);
+	} else {
+		await message.reply(content);
+	}
+}
+
+async function createThreadAndReply(
+	channel: BaseGuildTextChannel,
+	message: Message,
+	content: string,
+) {
+	let threadName =
+		message.content.slice(0, 50) +
+		(message.content.length > 50 ? "[...]" : "");
+	const thread = await channel.threads.create({
+		name: threadName,
+		autoArchiveDuration: ThreadAutoArchiveDuration.OneHour,
+		reason: "IS THAT CLYDE?",
+	});
+
+	await thread.send(content);
+}
+
+function isValidImage(contentType: string | null): boolean {
+	return ["image/png", "image/jpeg", "image/jpg"].includes(contentType || "");
 }
