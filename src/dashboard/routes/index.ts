c901c54867c2fade0455ba94d20e74608c14ad67
KorrumzPL <korrumzprivate@gmail.com>
2025-11-02 14:03:07 +0100

    fix: remove generating useless state

--- src/dashboard/routes/index.ts
+++ src/dashboard/routes/index.ts
@@ -6,10 +6,9 @@ export default async function index(fastify: FastifyInstance) {
 		const session = await auth.api.getSession({
 			headers: req.headers,
 		});
-		const state = crypto.randomBytes(32).toString("hex");
 
 		if (!session) {
-			return reply.redirect(`${process.env.OAUTH_URL}&state=${state}`);
+			return reply.redirect(`${process.env.OAUTH_URL}`);
 		}
 		reply.redirect("http://localhost:5173/dashboard/guild");
 	});
