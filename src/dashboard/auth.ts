de8d55776e3c5a021f4b3b85ed01cf5a03d234e6
KorrumzPL <korrumzprivate@gmail.com>
2025-11-08 14:44:55 +0100

    test

--- src/dashboard/auth.ts
+++ src/dashboard/auth.ts
@@ -76,7 +76,6 @@ export const auth = betterAuth({
         useSecureCookies: true,
         crossSubDomainCookies: {
             enabled: true,
-            domain: process.env.OAUTH_DOMAIN as string,
         },
 	},
 	session: {
