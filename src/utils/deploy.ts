75ca17aedf627abe39126e253d30d6e7e3ebe914
KorrumzPL <korrumzprivate@gmail.com>
2025-11-30 21:53:49 +0100

    fix: deploying commands

--- src/utils/deploy.ts
+++ src/utils/deploy.ts
@@ -8,29 +8,30 @@ const parsedCommands: SlashCommandBuilder[] = [];
 export async function deploy(client: SkyndalexClient) {
 	const commands = client.commands;
 	if (parsedCommands.length === 0)
-		commands.forEach(async (cmd, key) => {
-			if (key.includes("/")) {
-				const [name, subcommand] = key.split("/");
-				if (subcommand !== "index") return;
-				const subcommands = commands.filter(
-					(_value, key) =>
-						key.startsWith(`${name}/`) && key !== `${name}/index`,
-				);
-				const command = cmd.data;
-				for (const subcmd of subcommands.values()) {
-					command.addSubcommand(
-						subcmd.data as unknown as SlashCommandSubcommandBuilder,
-					);
-				}
-				parsedCommands.push(command);
-			} else {
-				parsedCommands.push(cmd.data);
-			}
-		});
+        if (parsedCommands.length === 0) {
+            for (const [key, cmd] of commands.entries()) {
+                if (key.includes("/")) {
+                    const [name, subcommand] = key.split("/");
+                    if (subcommand !== "index") continue;
+                    const subcommands = commands.filter(
+                        (_value, k) => k.startsWith(`${name}/`) && k !== `${name}/index`,
+                    );
+                    const command = cmd.data;
+                    for (const subcmd of subcommands.values()) {
+                        command.addSubcommand(
+                            subcmd.data as unknown as SlashCommandSubcommandBuilder,
+                        );
+                    }
+                    parsedCommands.push(command);
+                } else {
+                    parsedCommands.push(cmd.data);
+                }
+            }
+        }
 
-	client.rest.put(`/applications/${client.user?.id}/commands`, {
-		body: parsedCommands,
-	});
+	await client.rest.put(`/applications/${client.user?.id}/commands`, {
+        body: parsedCommands,
+    });
 
 	return commands;
 }
